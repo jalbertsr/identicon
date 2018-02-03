f3e0078cc9dfdd11adcc26fb27890b8c67fd649a
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-02-03 20:13:31 +0100

    build grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,30 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
 
-  @doc """
-  Hello world.
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
 
-  ## Examples
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
 
-      iex> Identicon.hello
-      :world
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
