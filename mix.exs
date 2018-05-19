6e2c46393a258b0774c763af15279e99b8f7f295
Joan Albert Segura <joan.rueda@kiwi.com>
2018-05-19 15:51:47 +0200

    exs publish info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
+      name: "Identicon",
+      source_url: "https://github.com/jalbertsr/identicon"
     ]
   end
 
@@ -18,10 +22,26 @@ defmodule Identicon.MixProject do
     ]
   end
 
+  defp description() do
+    "Given a string it returns a profile identicon image. "
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:egd, github: "erlang/egd"}
+    ]
+  end
+
+   defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "postgrex",
+      # These are the default files included in the package
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Joan Albert Segura"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jalbertsr/identicon"}
     ]
   end
 end
